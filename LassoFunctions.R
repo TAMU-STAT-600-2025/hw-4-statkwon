7a743c911087bf5164f7d9c92631d272928a3a1a
statkwon <statkwon96@gmail.com>
2025-10-09 17:33:33 -0500

    [Refactor] applied formatter

--- LassoFunctions.R
+++ LassoFunctions.R
@@ -1,7 +1,7 @@
 # [ToDo] Standardize X and Y: center both X and Y; scale centered X
 # X - n x p matrix of covariates
 # Y - n x 1 response vector
-standardizeXY <- function(X, Y){
+standardizeXY <- function(X, Y) {
   # [ToDo] Center Y
   
   # [ToDo] Center and scale X
@@ -13,13 +13,19 @@ standardizeXY <- function(X, Y){
   # Ymean - the mean of original Y
   # Xmeans - means of columns of X (vector)
   # weights - defined as sqrt(X_j^{\top}X_j/n) after centering of X but before scaling
-  return(list(Xtilde = Xtilde, Ytilde = Ytilde, Ymean = Ymean, Xmeans = Xmeans, weights = weights))
+  return(list(
+    Xtilde = Xtilde,
+    Ytilde = Ytilde,
+    Ymean = Ymean,
+    Xmeans = Xmeans,
+    weights = weights
+  ))
 }
 
-# [ToDo] Soft-thresholding of a scalar a at level lambda 
+# [ToDo] Soft-thresholding of a scalar a at level lambda
 # [OK to have vector version as long as works correctly on scalar; will only test on scalars]
-soft <- function(a, lambda){
-
+soft <- function(a, lambda) {
+  
 }
 
 # [ToDo] Calculate objective function of lasso given current values of Xtilde, Ytilde, beta and lambda
@@ -27,8 +33,8 @@ soft <- function(a, lambda){
 # Ytilde - centered Y, n x 1
 # lamdba - tuning parameter
 # beta - value of beta at which to evaluate the function
-lasso <- function(Xtilde, Ytilde, beta, lambda){
- 
+lasso <- function(Xtilde, Ytilde, beta, lambda) {
+  
 }
 
 # [ToDo] Fit LASSO on standardized data for a given lambda
@@ -37,21 +43,25 @@ lasso <- function(Xtilde, Ytilde, beta, lambda){
 # lamdba - tuning parameter
 # beta_start - p vector, an optional starting point for coordinate-descent algorithm
 # eps - precision level for convergence assessment, default 0.001
-fitLASSOstandardized <- function(Xtilde, Ytilde, lambda, beta_start = NULL, eps = 0.001){
+fitLASSOstandardized <- function(Xtilde,
+                                 Ytilde,
+                                 lambda,
+                                 beta_start = NULL,
+                                 eps = 0.001) {
   #[ToDo]  Check that n is the same between Xtilde and Ytilde
   
   #[ToDo]  Check that lambda is non-negative
   
-  #[ToDo]  Check for starting point beta_start. 
+  #[ToDo]  Check for starting point beta_start.
   # If none supplied, initialize with a vector of zeros.
   # If supplied, check for compatibility with Xtilde in terms of p
   
-  #[ToDo]  Coordinate-descent implementation. 
+  #[ToDo]  Coordinate-descent implementation.
   # Stop when the difference between objective functions is less than eps for the first time.
   # For example, if you have 3 iterations with objectives 3, 1, 0.99999,
   # your should return fmin = 0.99999, and not have another iteration
   
-  # Return 
+  # Return
   # beta - the solution (a vector)
   # fmin - optimal function value (value of objective at beta, scalar)
   return(list(beta = beta, fmin = fmin))
@@ -64,9 +74,13 @@ fitLASSOstandardized <- function(Xtilde, Ytilde, lambda, beta_start = NULL, eps 
 # n_lambda - length of desired tuning parameter sequence,
 #             is only used when the tuning sequence is not supplied by the user
 # eps - precision level for convergence assessment, default 0.001
-fitLASSOstandardized_seq <- function(Xtilde, Ytilde, lambda_seq = NULL, n_lambda = 60, eps = 0.001){
+fitLASSOstandardized_seq <- function(Xtilde,
+                                     Ytilde,
+                                     lambda_seq = NULL,
+                                     n_lambda = 60,
+                                     eps = 0.001) {
   # [ToDo] Check that n is the same between Xtilde and Ytilde
- 
+  
   # [ToDo] Check for the user-supplied lambda-seq (see below)
   # If lambda_seq is supplied, only keep values that are >= 0,
   # and make sure the values are sorted from largest to smallest.
@@ -74,20 +88,24 @@ fitLASSOstandardized_seq <- function(Xtilde, Ytilde, lambda_seq = NULL, n_lambda
   # print the warning message and proceed as if the values were not supplied.
   
   
-  # If lambda_seq is not supplied, calculate lambda_max 
+  # If lambda_seq is not supplied, calculate lambda_max
   # (the minimal value of lambda that gives zero solution),
   # and create a sequence of length n_lambda as
   lambda_seq = exp(seq(log(lambda_max), log(0.01), length = n_lambda))
   
-  # [ToDo] Apply fitLASSOstandardized going from largest to smallest lambda 
-  # (make sure supplied eps is carried over). 
+  # [ToDo] Apply fitLASSOstandardized going from largest to smallest lambda
+  # (make sure supplied eps is carried over).
   # Use warm starts strategy discussed in class for setting the starting values.
   
   # Return output
   # lambda_seq - the actual sequence of tuning parameters used
   # beta_mat - p x length(lambda_seq) matrix of corresponding solutions at each lambda value
   # fmin_vec - length(lambda_seq) vector of corresponding objective function values at solution
-  return(list(lambda_seq = lambda_seq, beta_mat = beta_mat, fmin_vec = fmin_vec))
+  return(list(
+    lambda_seq = lambda_seq,
+    beta_mat = beta_mat,
+    fmin_vec = fmin_vec
+  ))
 }
 
 # [ToDo] Fit LASSO on original data using a sequence of lambda values
@@ -96,12 +114,16 @@ fitLASSOstandardized_seq <- function(Xtilde, Ytilde, lambda_seq = NULL, n_lambda
 # lambda_seq - sequence of tuning parameters, optional
 # n_lambda - length of desired tuning parameter sequence, is only used when the tuning sequence is not supplied by the user
 # eps - precision level for convergence assessment, default 0.001
-fitLASSO <- function(X ,Y, lambda_seq = NULL, n_lambda = 60, eps = 0.001){
+fitLASSO <- function(X ,
+                     Y,
+                     lambda_seq = NULL,
+                     n_lambda = 60,
+                     eps = 0.001) {
   # [ToDo] Center and standardize X,Y based on standardizeXY function
- 
+  
   # [ToDo] Fit Lasso on a sequence of values using fitLASSOstandardized_seq
   # (make sure the parameters carry over)
- 
+  
   # [ToDo] Perform back scaling and centering to get original intercept and coefficient vector
   # for each lambda
   
@@ -109,7 +131,11 @@ fitLASSO <- function(X ,Y, lambda_seq = NULL, n_lambda = 60, eps = 0.001){
   # lambda_seq - the actual sequence of tuning parameters used
   # beta_mat - p x length(lambda_seq) matrix of corresponding solutions at each lambda value (original data without center or scale)
   # beta0_vec - length(lambda_seq) vector of intercepts (original data without center or scale)
-  return(list(lambda_seq = lambda_seq, beta_mat = beta_mat, beta0_vec = beta0_vec))
+  return(list(
+    lambda_seq = lambda_seq,
+    beta_mat = beta_mat,
+    beta0_vec = beta0_vec
+  ))
 }
 
 
@@ -119,11 +145,17 @@ fitLASSO <- function(X ,Y, lambda_seq = NULL, n_lambda = 60, eps = 0.001){
 # lambda_seq - sequence of tuning parameters, optional
 # n_lambda - length of desired tuning parameter sequence, is only used when the tuning sequence is not supplied by the user
 # k - number of folds for k-fold cross-validation, default is 5
-# fold_ids - (optional) vector of length n specifying the folds assignment (from 1 to max(folds_ids)), if supplied the value of k is ignored 
+# fold_ids - (optional) vector of length n specifying the folds assignment (from 1 to max(folds_ids)), if supplied the value of k is ignored
 # eps - precision level for convergence assessment, default 0.001
-cvLASSO <- function(X ,Y, lambda_seq = NULL, n_lambda = 60, k = 5, fold_ids = NULL, eps = 0.001){
+cvLASSO <- function(X ,
+                    Y,
+                    lambda_seq = NULL,
+                    n_lambda = 60,
+                    k = 5,
+                    fold_ids = NULL,
+                    eps = 0.001) {
   # [ToDo] Fit Lasso on original data using fitLASSO
- 
+  
   # [ToDo] If fold_ids is NULL, split the data randomly into k folds.
   # If fold_ids is not NULL, split the data according to supplied fold_ids.
   
@@ -131,7 +163,7 @@ cvLASSO <- function(X ,Y, lambda_seq = NULL, n_lambda = 60, k = 5, fold_ids = NU
   # and perform any additional calculations needed for CV(lambda) and SE_CV(lambda)
   
   # [ToDo] Find lambda_min
-
+  
   # [ToDo] Find lambda_1SE
   
   
@@ -145,6 +177,16 @@ cvLASSO <- function(X ,Y, lambda_seq = NULL, n_lambda = 60, k = 5, fold_ids = NU
   # lambda_1se - selected lambda based on 1SE rule
   # cvm - values of CV(lambda) for each lambda
   # cvse - values of SE_CV(lambda) for each lambda
-  return(list(lambda_seq = lambda_seq, beta_mat = beta_mat, beta0_vec = beta0_vec, fold_ids = fold_ids, lambda_min = lambda_min, lambda_1se = lambda_1se, cvm = cvm, cvse = cvse))
+  return(
+    list(
+      lambda_seq = lambda_seq,
+      beta_mat = beta_mat,
+      beta0_vec = beta0_vec,
+      fold_ids = fold_ids,
+      lambda_min = lambda_min,
+      lambda_1se = lambda_1se,
+      cvm = cvm,
+      cvse = cvse
+    )
+  )
 }
-
